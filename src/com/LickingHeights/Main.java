f8e351aa854e024adff8d22c5aeac6a5c2e0598c
Jacob Johnston <jacob.johnston.2022@student.lhschools.org>
2019-02-26 14:08:41 -0500

    19-33 Asked user for number did first ELSE IF statement

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -8,11 +8,25 @@ public class Main {
         Scanner keyboard;
         keyboard = new Scanner(System.in);
         String userName;
+        int userNumber;
+
 
 
         System.out.print("Hello what is your name ?");
         userName=keyboard.nextLine();
+        System.out.println("");
         System.out.println("Hello "+userName+" I am a Magic 8 Ball.");
-        System.out.println("So");
+        System.out.println("");
+        System.out.println("If you give me numbers I can tell you an");
+        System.out.println("outlook I see.");
+        System.out.println("So can you give me a number between 1-100.");
+        userNumber=keyboard.nextInt();
+        if (userNumber<10){
+            System.out.println("the number you chose was under ten");
+        }
+            else{
+            System.out.println("The number you choose was greater then 10");
+        }
+
     }
 }
