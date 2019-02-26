37f4c6113f9b200cac57a2300de7ede0db4551f9
Jacob Johnston <jacob.johnston.2022@student.lhschools.org>
2019-02-26 13:39:38 -0500

    1-19 Set scanner asked user name

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,9 +1,18 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.print("hgvmhgvmhgvg");
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        String userName;
+
+
+        System.out.print("Hello what is your name ?");
+        userName=keyboard.nextLine();
+        System.out.println("Hello "+userName+" I am a Magic 8 Ball.");
+        System.out.println("So");
     }
 }
