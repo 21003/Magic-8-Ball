ba51de32089e751fff523c7fb98fc1eb4db07166
Jacob Johnston <jacob.johnston.2022@student.lhschools.org>
2019-02-28 13:27:13 -0500

    33-39 added second ELSE IF statement

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -11,22 +11,28 @@ public class Main {
         int userNumber;
 
 
-
         System.out.print("Hello what is your name ?");
-        userName=keyboard.nextLine();
+        userName = keyboard.nextLine();
         System.out.println("");
-        System.out.println("Hello "+userName+" I am a Magic 8 Ball.");
+        System.out.println("Hello " + userName + " I am a Magic 8 Ball.");
         System.out.println("");
         System.out.println("If you give me numbers I can tell you an");
         System.out.println("outlook I see.");
         System.out.println("So can you give me a number between 1-100.");
-        userNumber=keyboard.nextInt();
-        if (userNumber<10){
-            System.out.println("the number you chose was under ten");
+        userNumber = keyboard.nextInt();
+        if (userNumber <= 10) {
+            System.out.println("You will have a very lucky day "); // Positive 1
         }
-            else{
-            System.out.println("The number you choose was greater then 10");
-        }
+        if (userNumber > 10 && userNumber <= 20) {
 
+            System.out.println("Your day will be HORRIBLE good luck ");
+        }
+        
     }
 }
+
+
+
+
+
+
