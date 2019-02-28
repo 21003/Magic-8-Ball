8695e29c1ae05e2767b7e7175b75a4c4e8841faa
Jacob Johnston <jacob.johnston.2022@student.lhschools.org>
2019-02-28 14:05:32 -0500

    53-69 finished ELSE IF statements

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -11,7 +11,7 @@ public class Main {
         int userNumber;
 
 
-        System.out.print("Hello what is your name ?");
+        System.out.println("Hello what is your name ?");
         userName = keyboard.nextLine();
         System.out.println("");
         System.out.println("Hello " + userName + " I am a Magic 8 Ball.");
@@ -33,11 +33,27 @@ public class Main {
             System.out.println("Some time soon you will step on a lego ");// Negative 2
         }
         if (userNumber > 40 && userNumber <= 50) {
-            System.out.println("Today you find some money when you clean your room");//Postive 3
+            System.out.println("Today you find some money when you clean your room"); // Positive 3
         }
-        if (userNumber > 50 && userNumber<=60) {
-            System.out.println("A fastfood employee will mess up your order");//Negative 3
+        if (userNumber > 50 && userNumber <= 60) {
+            System.out.println("A fastfood employee will mess up your order"); // Negative 3
         }
+        if (userNumber > 60 && userNumber <= 70) {
+            System.out.println("You will receive 5 chicken nuggets in 4 count box "); // Positive 4
+        }
+        if (userNumber > 70 && userNumber <= 80 ) {
+            System.out.println("You will ask for fastfood and your parents will say no"); // Negative 4
+        }
+        if (userNumber > 80 && userNumber <= 90 ) {
+            System.out.println("You will get 98% on the test you didn't study for"); // Positive 5
+        }
+        if (userNumber > 90 && userNumber <= 100) {
+            System.out.println("The Feds mixed up some phone #'s and now your facing 30 to life!! whoops !!"); // Negative 5
+        }
+        if (userNumber > 101) {
+            System.out.println("You are the chosen one, a rule breaker and you will have the best luck you can imagine ");
+        }
+
 
         }
         }
